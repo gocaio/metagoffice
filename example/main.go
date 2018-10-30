2e0f39536014b3a6d4d7efd416400fce66bf6370
Kevin <kevin.borras.soler@gmail.com>
2018-10-30 10:26:05 +0100

    Update main.go
    gofmt -s

--- example/main.go
+++ example/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"fmt"
+        "fmt"
 
-	"github.com/kevinborras/metagoffice"
+        "github.com/kevinborras/metagoffice"
 )
 
 func main() {
-	
-	content := metagoffice.GetContent("example/document.docx")
 
-	fmt.Println("Title: ", content.Title)
-	fmt.Println("Subject: ", content.Subject)
-	fmt.Println("Creator: ", content.Creator)
-	fmt.Println("Keywords: ", content.Keywords)
-	fmt.Println("Description: ", content.Description)
-	fmt.Println("Last modified by: ", content.LastModifiedBy)
-	fmt.Println("Revision: ", content.Revision)
-	fmt.Println("Created: ", content.Created)
-	fmt.Println("Modified: ", content.Modified)
-	fmt.Println("Category: ", content.Category)
+        content := metagoffice.GetContent("example/document.docx")
+
+        fmt.Println("Title: ", content.Title)
+        fmt.Println("Subject: ", content.Subject)
+        fmt.Println("Creator: ", content.Creator)
+        fmt.Println("Keywords: ", content.Keywords)
+        fmt.Println("Description: ", content.Description)
+        fmt.Println("Last modified by: ", content.LastModifiedBy)
+        fmt.Println("Revision: ", content.Revision)
+        fmt.Println("Created: ", content.Created)
+        fmt.Println("Modified: ", content.Modified)
+        fmt.Println("Category: ", content.Category)
 
 }
