6aefbdab23f6c409a37dcc2399c6f4016a0de775
kevinborras <kevin.borras.soler@gmail.com>
2018-11-28 15:20:15 +0100

    format

--- example/main.go
+++ example/main.go
@@ -18,24 +18,24 @@
 package main
 
 import (
-        "fmt"
+	"fmt"
 
-        "github.com/kevinborras/metagoffice"
+	"github.com/kevinborras/metagoffice"
 )
 
 func main() {
 
-        content := metagoffice.GetContent("example/document.docx")
+	content := metagoffice.GetContent("example/document.docx")
 
-        fmt.Println("Title: ", content.Title)
-        fmt.Println("Subject: ", content.Subject)
-        fmt.Println("Creator: ", content.Creator)
-        fmt.Println("Keywords: ", content.Keywords)
-        fmt.Println("Description: ", content.Description)
-        fmt.Println("Last modified by: ", content.LastModifiedBy)
-        fmt.Println("Revision: ", content.Revision)
-        fmt.Println("Created: ", content.Created)
-        fmt.Println("Modified: ", content.Modified)
-        fmt.Println("Category: ", content.Category)
+	fmt.Println("Title: ", content.Title)
+	fmt.Println("Subject: ", content.Subject)
+	fmt.Println("Creator: ", content.Creator)
+	fmt.Println("Keywords: ", content.Keywords)
+	fmt.Println("Description: ", content.Description)
+	fmt.Println("Last modified by: ", content.LastModifiedBy)
+	fmt.Println("Revision: ", content.Revision)
+	fmt.Println("Created: ", content.Created)
+	fmt.Println("Modified: ", content.Modified)
+	fmt.Println("Category: ", content.Category)
 
 }
--- test/metagoffice_test.go
+++ test/metagoffice_test.go
@@ -18,64 +18,64 @@
 package test
 
 import (
-        "reflect"
-        "testing"
+	"reflect"
+	"testing"
 
-        "github.com/kevinborras/metagoffice"
+	"github.com/kevinborras/metagoffice"
 )
 
 var docExpectedResult = metagoffice.XMLContent{
-        Title:          "Test Document",
-        Creator:        "Soler, Kevin",
-        Keywords:       "This is a tag",
-        Description:    "This is a comment",
-        LastModifiedBy: "Soler, Kevin",
-        Revision:       "4",
-        Created:        "2018-10-12T14:01:00Z",
-        Modified:       "2018-10-15T11:23:00Z",
+	Title:          "Test Document",
+	Creator:        "Soler, Kevin",
+	Keywords:       "This is a tag",
+	Description:    "This is a comment",
+	LastModifiedBy: "Soler, Kevin",
+	Revision:       "4",
+	Created:        "2018-10-12T14:01:00Z",
+	Modified:       "2018-10-15T11:23:00Z",
 }
 
 var excelExpectedResult = metagoffice.XMLContent{
-        Title:          "This is a title",
-        Creator:        "Soler, Kevin",
-        Keywords:       "1234",
-        LastModifiedBy: "Soler, Kevin",
-        Created:        "2018-10-15T10:51:30Z",
-        Modified:       "2018-10-15T10:52:41Z",
-        Category:       "Confidential",
+	Title:          "This is a title",
+	Creator:        "Soler, Kevin",
+	Keywords:       "1234",
+	LastModifiedBy: "Soler, Kevin",
+	Created:        "2018-10-15T10:51:30Z",
+	Modified:       "2018-10-15T10:52:41Z",
+	Category:       "Confidential",
 }
 
 var powerExpectedResult = metagoffice.XMLContent{
-        Title:          "PowerPoint Title",
-        Creator:        "Soler, Kevin",
-        Keywords:       "This is a tag",
-        LastModifiedBy: "Soler, Kevin",
-        Revision:       "1",
-        Created:        "2018-10-15T11:47:28Z",
-        Modified:       "2018-10-15T11:48:22Z",
-        Category:       "comercial",
+	Title:          "PowerPoint Title",
+	Creator:        "Soler, Kevin",
+	Keywords:       "This is a tag",
+	LastModifiedBy: "Soler, Kevin",
+	Revision:       "1",
+	Created:        "2018-10-15T11:47:28Z",
+	Modified:       "2018-10-15T11:48:22Z",
+	Category:       "comercial",
 }
 
 func TestDocxDocumment(t *testing.T) {
-        actualResult := metagoffice.GetContent("document.docx")
+	actualResult := metagoffice.GetContent("document.docx")
 
-        if !reflect.DeepEqual(docExpectedResult, actualResult) {
-                t.Fatalf("Expected %s but got %s", docExpectedResult, actualResult)
-        }
+	if !reflect.DeepEqual(docExpectedResult, actualResult) {
+		t.Fatalf("Expected %s but got %s", docExpectedResult, actualResult)
+	}
 }
 
 func TestExcelDocumment(t *testing.T) {
-        actualResult := metagoffice.GetContent("Book1.xslx")
+	actualResult := metagoffice.GetContent("Book1.xslx")
 
-        if !reflect.DeepEqual(excelExpectedResult, actualResult) {
-                t.Fatalf("Expected %s but got %s", excelExpectedResult, actualResult)
-        }
+	if !reflect.DeepEqual(excelExpectedResult, actualResult) {
+		t.Fatalf("Expected %s but got %s", excelExpectedResult, actualResult)
+	}
 }
 
 func TestPowerDocumment(t *testing.T) {
-        actualResult := metagoffice.GetContent("Presentation1.pptx")
+	actualResult := metagoffice.GetContent("Presentation1.pptx")
 
-        if !reflect.DeepEqual(powerExpectedResult, actualResult) {
-                t.Fatalf("Expected %s but got %s", powerExpectedResult, actualResult)
-        }
+	if !reflect.DeepEqual(powerExpectedResult, actualResult) {
+		t.Fatalf("Expected %s but got %s", powerExpectedResult, actualResult)
+	}
 }
